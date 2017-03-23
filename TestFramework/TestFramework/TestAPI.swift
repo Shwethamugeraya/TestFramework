39f3d6c9125f74bb7b0bd0f9243be8ba6b7e8174
Shwetha Mugeraya <smugeraya@sapient.com>
2017-03-23 15:53:52 +0530

    Committed test framework

---
+++ TestFramework/TestFramework/TestAPI.swift
@@ -0,0 +1,16 @@
+//
+//  TestAPI.swift
+//  TestFramework
+//
+//  Created by Shwetha Mugeraya on 3/23/17.
+//  Copyright © 2017 Shwetha Mugeraya. All rights reserved.
+//
+
+import Foundation
+
+public class TestAPI:NSObject {
+
+    public static func testMethod() {
+        print("Test Method Called")
+    }
+}
